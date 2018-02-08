a90d83048f792965634b96f41d46eb87f48d0ee5
Greg Zynda <zyndagj@gmail.com>
2018-02-07 21:03:05 -0600

    Added tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -35,6 +35,38 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ###############################################################################
 
-from k-omplex import main
+import unittest
+try:
+	from unittest.mock import patch
+except:
+	from mock import patch
+from os import path
+import komplexity
 
-main()
+class TestKomplexity(unittest.TestCase):
+	def setUp(self):
+		self.fasta = path.join(path.dirname(__file__),'test.fasta')
+	def test_1(self):
+		testArgs = ['-F',self.fasta,'-k','3','-w','5','-s','5']
+		patch(sys, 'argv', testArgs)
+		answer1 = \
+'''
+test1	0	5
+test1	5	10
+test1	10	15
+test1	13	18
+test2	0	5
+test2	5	10
+test2	8	13
+'''
+		assertMultiLineEqual(answer1, komplexity.main)
+
+if __name__ == "__main__":
+	unittest.main()
+
+#>test1
+#AAGGCCTTAGCTAGGCCT
+#>test2
+#TTCCGGAATCGAT
+#test1	18	7	18	19
+#test2	13	33	13	14
